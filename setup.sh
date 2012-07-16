b40c4cc8392ffc13188759b61aca0ed2822a6510
Tom Howe <turtlebender@gmail.com>
2012-07-16 11:22:24 -0500

    adding setup.sh file

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+(test -e ~/.bashrc && test ! -L ~/.bashrc) && rm ~/.bashrc
+test -L ~/.bashrc || ln -s ${pwd}/bashrc ~/.bashrc
+
+(test -e ~/.vimrc && test ! -L ~/.vimrc) && rm ~/.vimrc
+test -L ~/.vimrc || ln -s ${pwd}/vimrc ~/.vimrc
+
+(test -e ~/.vim && test ! -L ~/.vim) && rm -r ~/.vim
+test -L ~/.vim || ln -s ${pwd}/vim ~/.vim
+
