27252f9d243a6fd3fe38cfaf11bd834b2f28ec17
Tom Howe <turtlebender@gmail.com>
2012-07-17 09:25:48 -0500

    updating virtualenv installation

--- setup.sh
+++ setup.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
 
-(test -e ~/.bashrc && test ! -L ~/.bashrc) && rm ~/.bashrc
-test -L ~/.bashrc || ln -s $(pwd)/bashrc ~/.bashrc
-
-(test -e ~/.vimrc && test ! -L ~/.vimrc) && rm ~/.vimrc
-test -L ~/.vimrc || ln -s $(pwd)/vimrc ~/.vimrc
-
-(test -e ~/.vim && test ! -L ~/.vim) && rm -r ~/.vim
-test -L ~/.vim || ln -s $(pwd)/vim ~/.vim
-
-(test -e ~/.git_tools && test ! -L ~/.git_tools) && rm -r ~/.git_tools
-test -L ~/.git_tools || ln -s $(pwd)/git_tools ~/.git_tools
+for i in * 
+do
+  (test -e ~/.$i && test ! -L ~/.$i) && rm ~/.$i
+  test -L ~/.$i || ln -s $(pwd)/$i ~/.$i
+done
 test -d ~/.git_tools/vendor || mkdir ~/.git_tools/vendor
-test -L ~/.git_tools/vendor/virtualenv.py || ln -s $(pwd)/virtualenv.py ~/.git_tools/vendor/virtualenv.py
 test -e ~/.git_tools/vendor/bin/python || python ~/.git_tools/vendor/virtualenv.py --distribute ~/.git_tools/vendor/python
 PIP_DOWNLOAD_CACHE=~/.git_tools/vendor/cache ~/.git_tools/vendor/python/bin/pip install gitpython jinja2
-
-(test -e ~/.gitconfig && test ! -L ~/.gitconfig) && rm ~/.gitconfig
-test -L ~/.gitconfig || ln -s $(pwd)/gitconfig ~/.gitconfig
+rm -f virtualenv.py*
+rm -f distribute*
