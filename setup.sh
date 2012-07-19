af90522f9c4ebb81adc8e1e4a351fc6ade36fe80
Tom Howe <turtlebender@gmail.com>
2012-07-19 18:59:34 -0500

    adding food critic

--- setup.sh
+++ setup.sh
@@ -6,7 +6,19 @@ do
   test -L ~/.$i || ln -s $(pwd)/$i ~/.$i
 done
 test -d ~/.git_tools/vendor || mkdir ~/.git_tools/vendor
-test -e ~/.git_tools/vendor/bin/python || python ~/.git_tools/vendor/virtualenv.py --distribute ~/.git_tools/vendor/python
+test -e ~/.git_tools/vendor/bin/python || python ./vendor/virtualenv.py --distribute ~/.git_tools/vendor/python
 PIP_DOWNLOAD_CACHE=~/.git_tools/vendor/cache ~/.git_tools/vendor/python/bin/pip install gitpython jinja2
+python ./vendor/virtualenv.py --distribute $(pwd)/tools/flake8
+pushd $(pwd)/tools/flake8
+$(pwd)/bin/python $(pwd)/setup.py install
+popd
+test -d ~/tools || ln -s $(pwd)/tools ~/tools
 rm -f virtualenv.py*
 rm -f distribute*
+test -d ~/bin || mkdir ~/bin
+test -L ~/bin/flake8 || ln -s $(pwd)/tools/flake8/bin/flake8 ~/bin/flake8
+
+pushd tools/foodcritic
+bundle install --local --path vendor
+popd
+test -L ~/bin/foodcritic || ln -s ~/tools/foodcritic/foodcritic ~/bin/foodcritic
